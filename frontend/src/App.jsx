8c31d5d6d11d143e827de5e5dec088edec76bf38
Carlos Silva <55637586+carlossilva22@users.noreply.github.com>
2025-06-20 23:09:24 +0100

    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext';
+
+//import Signup from './pages/signup/Signup';
+
+export default function App() {
+  const { authUser } = useAuthContext();
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route>
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/" /> : <Signup />}
+          />
+        </Route>
+      </Routes>
+      <Toaster />
+    </div>
+  );
+}
